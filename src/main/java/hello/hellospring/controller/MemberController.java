fcf2babe3f2b4286e5fa445e8ddab7513dac2619
mitsui3point <rladbsgh0331@gmail.com>
2022-10-29 10:32:58 +0900

    refactor(MemberController, HelloController): modify
    add HelloController
    - static resource, MVC getMapping, string test api, json test api restore
    
    modify MemberController
    - remove HelloController module

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -17,42 +17,4 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "spring!!");
-        return "hello";
-    }
-
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello-template";
-    }
-
-    @GetMapping("hello-string")
-    @ResponseBody
-    public String helloString(@RequestParam("name") String name) {
-        return "hello " + name;
-    }
-
-    @GetMapping("hello-api")
-    @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name) {
-        Hello hello = new Hello();
-        hello.setName(name);
-        return hello;
-    }
-
-
-    static class Hello {
-        private String name;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
 }
