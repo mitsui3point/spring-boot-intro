d81fc2120ea8ddd1d69f6ed377ffe36d78ce72dd
mitsui3point <rladbsgh0331@gmail.com>
2022-11-01 13:46:11 +0900

    feat(SpringDataJpaMemberRepository): add
    add SpringDataJpaMemberRepository
    - to replace repository implements(Jpa -> Spring Data Jpa)
    
    feat(SpringConfig): update
    
    update SpringConfig
    - Constructor MemberRepository DI to replace repository implements(Jpa -> Spring Data Jpa)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -4,28 +4,22 @@ import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager entityManager;
+    private MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(entityManager);
+        MemberService memberService = new MemberService(memberRepository);
+        return memberService;
     }
 }
